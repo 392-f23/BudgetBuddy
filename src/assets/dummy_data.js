3e4b7f08ce4efa81d6d27a81c0354fcb6f59de35
DooHwanKim0419 <dkimh0419@gmail.com>
2023-10-08 16:10:01 -0500

    Changed dummy data

--- src/assets/dummy_data.js
+++ src/assets/dummy_data.js
@@ -3,20 +3,20 @@ export const dummyData = {
   Income: 3500,
   Budget: {
     Monthly: 3000,
-    Rent: 1600,
+    Rent: 1725,
     Food: 600,
-    Transport: 300,
+    Transport: 400,
   },
   Expenses: {
     Rent: {
-      total: 1725,
+      total: 1525,
       subExpense: {
         BaseRent: 1300,
         Utilities: 225,
       },
     },
     Food: {
-      total: 450,
+      total: 423,
       subExpense: {
         Groceries: 300,
         "Dine-Out": 123,
