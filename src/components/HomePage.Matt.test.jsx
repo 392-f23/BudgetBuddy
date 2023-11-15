af142d60b9e0408160d633f5996351c8ea3b19c6
MatthewChang908 <115748886+MatthewChang908@users.noreply.github.com>
2023-11-14 23:41:20 -0600

    started matt test

---
+++ src/components/HomePage.Matt.test.jsx
@@ -0,0 +1,9 @@
+import { it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from "../App" 
+
+describe("Check budget buddy image", () => {
+    it("When the expenses exceed budget, then the logo image should load “angry version” of BudgetBuddy icon", async () => {
+
+    }); 
+}); 
