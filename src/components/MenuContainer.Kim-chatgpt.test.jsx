1311a33d1698e9f7c413d9abfc4631d8da865121
DooHwanKim0419 <dkimh0419@gmail.com>
2023-11-15 15:19:19 -0600

    ChatGpt Unit Test Fix

--- src/components/MenuContainer.Kim-chatgpt.test.jsx
+++ src/components/MenuContainer.Kim-chatgpt.test.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { expect } from "vitest";
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import MenuContainer from "./MenuContainer";
 
 describe("MenuContainer Component", () => {
   it("renders all three menu options", () => {
-    const { getByText } = render(<MenuContainer />);
+    const customProps = {
+      data: {
+        budget: 0,
+      },
+      totalExpenses: 0,
+    };
+    const { getByText } = render(
+      <MemoryRouter>
+        <MenuContainer {...customProps} />
+      </MemoryRouter>
+    );
 
     // Check if "Home" menu is rendered
-    expect(getByText("Home")).toBeInTheDocument();
+    expect(getByText("Home")).toBeDefined();
 
     // Check if "Insights" menu is rendered
-    expect(getByText("Insights")).toBeInTheDocument();
+    expect(getByText("Insights")).toBeDefined();
 
     // Check if "Logout" menu is rendered
-    expect(getByText("Logout")).toBeInTheDocument();
+    expect(getByText("Logout")).toBeDefined();
   });
 });
