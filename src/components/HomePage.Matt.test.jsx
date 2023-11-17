c0a400ca12c75228b6c0c5b8cc372e87b39378c7
MatthewChang908 <115748886+MatthewChang908@users.noreply.github.com>
2023-11-16 21:47:54 -0600

    angry buddy

--- src/components/HomePage.Matt.test.jsx
+++ src/components/HomePage.Matt.test.jsx
@@ -1,9 +1,29 @@
-import { it } from 'vitest';
+import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from "../App" 
-
+import { MemoryRouter } from "react-router-dom";
+import MenuContainer
+ from './MenuContainer';
 describe("Check budget buddy image", () => {
-    it("When the expenses exceed budget, then the logo image should load “angry version” of BudgetBuddy icon", async () => {
+    it("Check evil budget buddy image renders if expenses = budget", async () => {
+        // Mocking or setting up necessary variables and functions
+        const expenses = 500; // Set expenses
+        const budget = 500; // Set budget
+        const logoEvil = "/src/assets/budget_buddy_evil.png"; 
+        // Rendering the component
+        render(
+          <MemoryRouter>
+            <MenuContainer data={{ budget }} totalExpenses={expenses} />
+          </MemoryRouter>
+        );
+  
+        // Get the logo image element
+        const logoImage = screen.getByRole("img");
 
-    }); 
-}); 
+        // Get the 'src' attribute of the logo image
+        const logoSrc = logoImage.getAttribute("src");
+
+        // Asserting that the logo image source matches the 'angry version' of BudgetBuddy icon
+        expect(logoSrc).toBe(logoEvil);
+
+    });
+  });
