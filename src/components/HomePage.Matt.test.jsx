572bb882e91e88cfe64c37e90282d5504a1b7bd4
MatthewChang908 <115748886+MatthewChang908@users.noreply.github.com>
2023-11-16 21:55:09 -0600

    updated for neutral and good budget buddy

--- src/components/HomePage.Matt.test.jsx
+++ src/components/HomePage.Matt.test.jsx
@@ -26,4 +26,50 @@ describe("Check budget buddy image", () => {
         expect(logoSrc).toBe(logoEvil);
 
     });
+
+    it("Check neutral budget buddy image renders if expenses are 2/3 of budget", () => {
+        // Mocking or setting up necessary variables and functions
+        const expenses = 300; // Set expenses different from budget
+        const budget = 500; // Set budget
+        const logoNeutral = "/src/assets/budget buddy.png"; 
+    
+        // Rendering the component
+        render(
+          <MemoryRouter>
+            <MenuContainer data={{ budget }} totalExpenses={expenses} />
+          </MemoryRouter>
+        );
+    
+        // Get the logo image element
+        const logoImage = screen.getByRole("img");
+    
+        // Get the 'src' attribute of the logo image
+        const logoSrc = logoImage.getAttribute("src");
+    
+        // Asserting that the logo image source does not match the 'evil' BudgetBuddy icon
+        expect(logoSrc).toBe(logoNeutral);
+      });
+      it("Check good budget buddy image renders if expenses are 1/3 of budget", () => {
+        // Mocking or setting up necessary variables and functions
+        const expenses = 100; // Set expenses different from budget
+        const budget = 300; // Set budget
+        const logoGood = "/src/assets/budget_buddy_good.png"; 
+    
+        // Rendering the component
+        render(
+          <MemoryRouter>
+            <MenuContainer data={{ budget }} totalExpenses={expenses} />
+          </MemoryRouter>
+        );
+    
+        // Get the logo image element
+        const logoImage = screen.getByRole("img");
+    
+        // Get the 'src' attribute of the logo image
+        const logoSrc = logoImage.getAttribute("src");
+    
+        // Asserting that the logo image source does not match the 'evil' BudgetBuddy icon
+        expect(logoSrc).toBe(logoGood);
+      });
+    
   });
